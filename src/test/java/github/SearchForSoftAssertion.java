11022599902be3fdcc9bfec51a0ed56d6028242f
George Sumnik <georgesumnik@gmail.com>
2023-02-05 12:36:30 -0500

    search for SoftAssertions

---
+++ src/test/java/github/SearchForSoftAssertion.java
@@ -0,0 +1,30 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SearchForSoftAssertion {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void searchJUnit5CodeTest() {
+
+        open("/selenide/selenide");
+        $("ul.UnderlineNav-body li.d-inline-flex a#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("div#wiki-content").shouldHave(text("2. Using JUnit4 just describe rule inside test class:"));
+
+    }
+}
