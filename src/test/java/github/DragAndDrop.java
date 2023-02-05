2fbea3c19978735410dd57b7e5693d02ebd5c8bd
George Sumnik <georgesumnik@gmail.com>
2023-02-05 16:41:40 -0500

    hover

--- src/test/java/github/DragAndDrop.java
+++ src/test/java/github/DragAndDrop.java
@@ -1,8 +1,34 @@
 package github;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class DragAndDrop {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
 
+    @Test
+    void searchJUnit5CodeTest() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+
+        actions().moveToElement($(byText("A"))).clickAndHold().moveByOffset(250, 0).release().perform();
+        sleep(3000);
+        $("#column-b").shouldHave(text("A"));
+        sleep(3000);
+
+
+
+    }
 
 
 }
