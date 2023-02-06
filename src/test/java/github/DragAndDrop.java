fcad114774236396124d15107551d6b85ea30ed7
George Sumnik <georgesumnik@gmail.com>
2023-02-06 06:36:43 -0500

    fix

--- src/test/java/github/DragAndDrop.java
+++ src/test/java/github/DragAndDrop.java
@@ -1,35 +1,21 @@
 package github;
-
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
-
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
     }
-
     @Test
     void searchJUnit5CodeTest() {
-
         open("https://the-internet.herokuapp.com/drag_and_drop");
-
-
        // actions().moveToElement($(byText("A"))).clickAndHold().moveByOffset(250, 0).release().perform();
-
         $("#column-a").dragAndDropTo($("#column-b"));
-
         $("#column-b").shouldHave(text("A"));
-
-
-
     }
-
-
 }
--- src/test/java/github/HoverForSolutions.java
+++ src/test/java/github/HoverForSolutions.java
@@ -1,36 +1,26 @@
 package github;
-
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HoverForSolutions {
 
+    SelenideElement title = $(".h1-mktg.mb-3");
+
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
     }
-
     @Test
     void searchJUnit5CodeTest() {
-
         open("/selenide/selenide");
-
-
-        // Подвести курсор
         $(byText("Solutions")).hover();
         $(byText("Enterprise")).click();
-        $(byText("Build like the best")).shouldBe(visible);
-
-
+        title.shouldHave(text("Build like the best"));
     }
-
-
-
 }
